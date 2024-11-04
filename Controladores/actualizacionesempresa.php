2f83c955ad19d239b499f8bea1456d81cd782c41
Gerardo Luis Quiroga León <isc20350309@gmail.com>
2024-11-03 19:48:31 -0500

    se agrego lo de Yuli

--- Controladores/actualizacionesempresa.php
+++ Controladores/actualizacionesempresa.php
@@ -17,7 +17,6 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar Información de la Empresa</title>
-    <link rel="icon" href="../img/vista.png" type="image/png">
     <style>
         /* Estilos generales del contenedor */
         body {
